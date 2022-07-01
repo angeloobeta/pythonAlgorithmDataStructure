7026d6f61e32385431d27713fb32157daa881f5b
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2022-07-01 11:50:50 +0100

    Project Euler question 3. Highest Prime_Factors

--- Project Euler/3. prime_factors.py
+++ Project Euler/3. prime_factors.py
@@ -1,12 +1,13 @@
 def is_prime(number):
     for i in range(3, number // 2, 2):
-        if number % i == 0: return False
+        if number % i == 0:
+            return False
     return True
 
 
 def prime_factor(number):
     maximum_number = 0
-    for i in range(3, number//2, 2):
+    for i in range(3, number // 2, 2):
         if number % i == 0 and is_prime(i):
             maximum_number = i
     return maximum_number
